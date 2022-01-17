09c05ccad38f4da022bdb185e1e1b68af61d91c9
Sulaiman Birashk <sully.birashk@gmail.com>
2022-01-17 14:15:25 -0800

    Feat: Key Shift value Test: Key Shift

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -8,6 +8,21 @@ class Decrypt
     @ciphertext = ciphertext
     @key = key
     @date = date
+    @key_hash = Hash.new (0)
+    @offset_hash = Hash.new (0)
+    @shift_amount = Hash.new (0)
+    @decrypted_hash = Hash.new(0)
+  end
+
+  def key_shift
+    seperated_key = @key.split('') # Returns ["1", "7", "3", "8", "0"]
+
+    @key_hash[:a] = seperated_key[0] + seperated_key[1]
+    @key_hash[:b] = seperated_key[1] + seperated_key[2]
+    @key_hash[:c] = seperated_key[2] + seperated_key[3]
+    @key_hash[:d] = seperated_key[3] + seperated_key[4]
+
+    return @key_hash
   end
 
 
--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 RSpec.describe Decrypt do
   before :each do
-    @decrypt = Decrypt.new("Test", "17380", "150122")
+    @decrypt = Decrypt.new("nekw", "17380", "150122")
   end
 
   it 'exists' do
@@ -12,7 +12,7 @@ RSpec.describe Decrypt do
   end
 
   it 'has a ciphertext' do
-    expect(@decrypt.ciphertext).to eq('Test')
+    expect(@decrypt.ciphertext).to eq('nekw')
   end
 
   it 'has a key' do
@@ -21,6 +21,18 @@ RSpec.describe Decrypt do
 
   it 'has a date' do
     expect(@decrypt.date).to eq("150122")
+  end
+
+  it 'can assight a - d a number called key' do
+    expect(@decrypt.key_shift).to eq({a: "17", b: "73", c: "38", d: "80"})
+  end
+
+  xit 'can assign a - d a number called offset' do
+    expect(@decrypt.offset_shift).to eq({a: "4", b: "8", c: "8", d: "4"})
+  end
+
+  xit 'can assign a number to a - d to know how many numbers to shift' do
+    expect(@decrypt.shift_amount).to eq({first: 21, second: 27, third: 19, fourth: 3})
   end
 
 end
